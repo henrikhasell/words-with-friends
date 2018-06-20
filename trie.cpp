1ab5727c5c46d6f5b72240f82b7a867f1af35fd5
Henrik Hasell <henrik@prosys.com>
2018-06-20 15:12:48 +0100

    Added trie class.

---
+++ trie.cpp
@@ -0,0 +1,56 @@
+#include "trie.hpp"
+
+bool Trie::contains(const std::string &string) const
+{
+	std::set<Segment>::iterator iterator = children.find(string[0]);
+
+	if(iterator != children.end())
+	{
+		const Segment &segment = *iterator;
+
+		if(string.length() == 1)
+		{
+			return segment.valid;
+		}
+
+		return segment.contains(string.substr(1));
+	}
+	
+	return false;
+}
+
+void Trie::insert(const std::string &string)
+{
+	std::set<Segment>::iterator iterator = children.find(string[0]);
+
+	if(iterator == children.end())
+	{
+		children.insert(string);
+	}
+	else
+	{
+		((Segment)*iterator).insert(string.substr(1));
+	}
+}
+
+Segment::Segment(char character) :
+	character(character)
+{
+	
+}
+
+Segment::Segment(const std::string &string) :
+	valid(string.length() == 1)
+{
+	if(!valid)
+	{
+		insert(string.substr(1));
+	}
+	
+	character = string[0];
+}
+
+bool Segment::operator< (const Segment &right) const
+{
+	return character < right.character;
+}	
---
+++ trie.hpp
@@ -0,0 +1,33 @@
+#ifndef TRIE_HPP
+#define TRIE_HPP
+
+#include <set>
+#include <string>
+
+#include <iostream>
+
+class Trie;
+class Segment;
+
+class Trie
+{
+public:
+	bool contains(const std::string &string) const;
+	void insert(const std::string &string);
+protected:
+	std::set<Segment> children;
+};
+
+class Segment : public Trie
+{
+	friend class Trie;
+public:
+	Segment(const std::string &string);
+	bool operator< (const Segment &right) const;
+protected:
+	Segment(char character);
+	char character;
+	bool valid;
+};
+
+#endif
